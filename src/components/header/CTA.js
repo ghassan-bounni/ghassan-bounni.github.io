4e3667496390049b25b2642ca5e428acf7912c5f
ghassan bounni <ghassan.bounni18@gmail.com>
2022-07-12 23:49:31 +0300

    update CTA

--- src/components/header/CTA.js
+++ src/components/header/CTA.js
@@ -1,8 +1,8 @@
-import CV from "../../assets/Ghassan_CV.PDF";
+import CV from "../../assets/cv_ghassan.pdf";
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} className="btn" download="Ghassan_CV.PDF">
+      <a href={CV} className="btn" download="cv_ghassan.pdf">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
